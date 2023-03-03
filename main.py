0b1c991f58c82e545a2651172e6d67ecceb2a88d
Zid95 <zid.abdulkarim@gmail.com>
2023-03-03 15:21:09 +0100

    finished

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
+from bs4 import BeautifulSoup
+import requests
+from csv import writer
+
+# add url
+url = "url"
+page = requests.get(url)
+
+soup = BeautifulSoup(page.content,'html.parser')
+
+# add html tags from website example: find_all('div' and class_='container')
+lists = soup.find_all('htmltag',class_="class name")
+
+# create file 
+with open('example.csv','w',encoding='utf8',newline='') as f:
+  thewriter = writer(f) 
+  header = ['title','description']
+  thewriter.writerow(header)
+
+
+  for item in lists:
+    title = item.find('htmltag',class_="class name").text().replace('\n','')
+
+    data = [title]
+    thewriter.writerow(data)
 
 
 
